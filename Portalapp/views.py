5ed6e2bd9317c68ffe1b9202a5b05ea5bf50f4f9
Akash Nair <akash30121998@hotmail.com>
2018-07-05 13:02:24 +0530

    Updated skeleton of Website

--- Portalapp/views.py
+++ Portalapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+import datetime
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'Portalapp/index.html'
